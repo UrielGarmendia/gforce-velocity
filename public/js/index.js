cb86f038bad88dba13df5256374dd199cdae6a45
UrielGarmendia <garmendiauriel@gmail.com>
2023-02-04 18:33:09 -0300

    hicimos el nav y el footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
-let buscador = document.getElementById('buscador');
+    const menuBars = document.querySelector('.icono-menu-mobile');
+    const menuMobile = document.querySelector('.menu-mobile');
+    let items = document.getElementsByClassName('navigation')
 
-buscador.addEventListener('mousein', () => {
-    buscador.style.backgroundColor = 'red';
+    menuBars.addEventListener('click', () => {
+    menuMobile.classList.toggle('oculto');
 });
+
+
+// for (let i = 0; i < items.length; i++) {
+//     items[i].addEventListener("mouseover", function (e) {
+//         this.style.color = "#252525";
+//         this.style.backgroundColor = "#c63637";
+//       });
+//       items[i].addEventListener("mouseleave", function (e) {
+//         this.style.color = "#252525";
+//         this.style.backgroundColor = "transparent";
+//       });
+// };
